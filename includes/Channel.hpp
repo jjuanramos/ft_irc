840aa24642e935288ffa3b3400ed0a928a36647a
jjuanramos <jjuanramos91@gmail.com>
2024-12-14 12:20:23 +0100

    made constructors public

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,14 +6,12 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 10:44:00 by juramos           #+#    #+#             */
-/*   Updated: 2024/11/29 11:41:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/12/14 10:45:48 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
-
-class Client;
 
 #include "IRC.hpp"
 #include "IRCTypes.hpp"
@@ -29,9 +27,6 @@ private:
     std::map<unsigned int, Client*> _clients;      // Clientes en el canal
     std::map<unsigned int, Client*> _operators;    // Operadores (true) y usuarios normales (false)
     size_t _userLimit;                         // Límite de usuarios (para modo l)
-    Channel(const Channel& other);
-    Channel& operator=(const Channel& other);
-    Channel(void);
     // Métodos auxiliares
     bool canModifyTopic(Client* client) const;
     bool isInviteOnly() const;
@@ -39,7 +34,10 @@ private:
 
 public:
     // Constructor y destructor
+    Channel(void);
+    Channel(const Channel& other);
     Channel(const std::string& name, Client* creator);
+    Channel& operator=(const Channel& other);
     ~Channel();
 
     // Getters básicos
